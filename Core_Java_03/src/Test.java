3c5b85dc52956fa6c5edc3220bbcccde4814abc6
LYSmaybe <623845536@qq.com>
2019-12-27 18:36:51 +0800

    Java核心卷1

---
+++ Core_Java_03/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args){
+        float x = 42.0;
+        int y = 42;
+        if(x == y)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
